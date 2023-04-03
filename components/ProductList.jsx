7d3dde6140e50d85cfc753cd3d2abdfd367c04a1
kartikpavan <kartik.varanasi@yahoo.com>
2023-04-03 23:59:24 +0530

    Fetching products from firebase firestore

--- components/ProductList.jsx
+++ components/ProductList.jsx
@@ -1,7 +1,9 @@
-import React, { useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 import { View } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import { products } from "../data/products";
+import { db } from "../firebase/firebase.config";
 import { getProducts } from "../redux/slice/productSlice";
 import SingleProduct from "./SingleProduct";
 
@@ -9,12 +11,18 @@ const ProductList = () => {
   const { product } = useSelector((store) => store.product);
   const dispatch = useDispatch();
 
+  const [items, setItems] = useState([]);
+
   useEffect(() => {
     if (product.length > 0) return; // to prevent duplication of items after each dispatch event is called.
-    const fetch_local_products = () => {
-      products.map((item) => dispatch(getProducts(item)));
+    const fetch_products = async () => {
+      const querySnapshot = await getDocs(collection(db, "types"));
+      querySnapshot.forEach((doc) => {
+        items.push(doc.data());
+      });
+      items?.map((p) => dispatch(getProducts(p)));
     };
-    fetch_local_products();
+    fetch_products();
   }, []);
 
   return (
